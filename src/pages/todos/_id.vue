49fa6c2e84f74a1218ef974a942c3b29ff91fab9
sj5466 <songsuji0@gmail.com>
2023-12-28 03:45:04 +0900

    todo에 loading 추가
    todo list 중 한 부분을 클릭하면 페이지가 이동하는데 화면 전환하는데 시간이 걸리면 loadingd이 보임

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>Todo Page</div>
+  <h1>Todo Page</h1>
+  <div v-if="loading">Loading...</div>
+  <form v-else>
+    <div class="form-group">
+      <label for="">Todo Subject</label>
+      <input v-model="todo.subject" type="text" class="form-control" />
+    </div>
+    <button class="btn btn-primary">저장</button>
+  </form>
 </template>
 
 <script>
 import { useRoute } from "vue-router";
+import { ref } from "@vue/reactivity";
+import axios from "axios";
 
 export default {
   setup() {
     const route = useRoute();
+    const todo = ref(null);
+    const loading = ref(true);
 
+    const getTodo = async () => {
+      const res = await axios.get(
+        "http://localhost:3000/todos/" + route.params.id
+      );
+
+      todo.value = res.data;
+      loading.value = false;
+      console.log(res);
+    };
     console.log(route.params.id);
+
+    getTodo();
+
+    return {
+      todo,
+      loading,
+    };
   },
 };
 </script>
